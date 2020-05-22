8982d6faded9247dde43587a90e2e372a4b1ab61
Your Name <you@example.com>
2020-05-23 01:43:41 +0400

    End Task

--- view/layout/body.php
+++ view/layout/body.php
@@ -2,8 +2,8 @@
 <html>
 <head>
     <title><?=(isset($pageTitle) ? $pageTitle : 'Пользователи')?></title>
-    <link rel="stylesheet" type="text/css" href="http://cdn.sencha.com/ext/gpl/4.2.0/resources/css/ext-all.css" />
-    <script type="text/javascript" src="http://cdn.sencha.com/ext/gpl/4.2.0/ext-all.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.sencha.com/ext/gpl/4.2.0/resources/css/ext-all.css" />
+    <script type="text/javascript" src="https://cdn.sencha.com/ext/gpl/4.2.0/ext-all.js"></script>
 </head>
 <body>
 <?=$content?>
